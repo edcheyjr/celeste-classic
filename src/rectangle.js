36b88b59f7c77ff9bc2189f295735cbaa24fa93f
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-11 14:12:55 +0300

    rectangle blueprint

---
+++ src/rectangle.js
@@ -0,0 +1,35 @@
+class Rectangle {
+  constructor(
+    canvasWidth,
+    canvasHeight,
+    width = 100,
+    height = 25,
+    color = '#fff',
+    speed = 5,
+    y = Math.floor(Math.random() * canvasHeight),
+    x
+  ) {
+    this.canvasHeight = canvasHeight
+    this.canvasWidth = canvasWidth
+    this.width = width
+    this.height = height
+    this.x = x || Math.floor(Math.random() * (this.canvasWidth / 2))
+    this.y = y || canvasHeight / 2 - height / 2
+    this.color = color
+    this.speed = speed
+  }
+  draw(ctx) {
+    ctx.save()
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+    ctx.restore()
+  }
+  update(deltaTime) {
+    // move from left to right and back to start
+    if (this.x > this.canvasWidth + this.width) {
+      this.x = -this.width
+      this.y = Math.floor(Math.random() * this.canvasHeight)
+    }
+    this.x = Math.floor(this.x + this.speed)
+  }
+}
