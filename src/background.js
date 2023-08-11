ebcbd36812cea36c18088e6d1b72a528b165afb5
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-11 14:13:03 +0300

    background class

---
+++ src/background.js
@@ -0,0 +1,55 @@
+class Background {
+  constructor(canvasWidth, canvasHeight, bgImage) {
+    this.x = 0
+    this.y = 0
+    this.canvasWidth = canvasWidth
+    this.canvasHeight = canvasHeight
+    this.bgImage = bgImage
+    this.totalRectangles = 20
+    this.rectangles = []
+    this.rectMaxSpeed = 12
+    this.rectMinSpeed = 5
+    this.#_intializeRectangles()
+  }
+  draw(ctx) {
+    // draw moving items heres
+    this.rectangles.forEach((rectangle) => {
+      rectangle.draw(ctx)
+    })
+
+    // still items
+    ctx.drawImage(
+      this.bgImage,
+      this.x,
+      this.y,
+      this.canvasWidth,
+      this.canvasHeight
+    )
+  }
+
+  update(deltaTime) {
+    // update
+    this.rectangles.forEach((rectangle) => {
+      rectangle.update(deltaTime)
+    })
+  }
+  #_intializeRectangles() {
+    for (let i = 0; i < this.totalRectangles; i++) {
+      let randomWidth = Math.floor(Math.random() * 150 + 80)
+      let randomRectSpeed = Math.floor(
+        Math.random() * this.rectMaxSpeed + this.rectMinSpeed
+      )
+      let randomHeight = Math.floor(Math.random() * 50 + 15)
+      this.rectangles.push(
+        new Rectangle(
+          this.canvasWidth,
+          this.canvasHeight,
+          randomWidth,
+          randomHeight,
+          '#1c2a4d',
+          randomRectSpeed
+        )
+      )
+    }
+  }
+}
