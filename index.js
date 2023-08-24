df8612441444193bea627c86a30da03087b952f3
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-24 22:52:50 +0300

    height and width canvas updated

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 // global variables
-
-const CANVAS_WIDTH = (canvas.width = 700)
-const CANVAS_HEIGHT = (canvas.height = 700)
+const GRAVITY = 1
+const CANVAS_WIDTH = (canvas.width = 760)
+const CANVAS_HEIGHT = (canvas.height = 760)
 
 //game size
 
@@ -18,6 +18,31 @@ bgImage.style.fill = '#847E87'
 // new bg
 let bg = new Background(CANVAS_WIDTH, CANVAS_HEIGHT, bgImage)
 let newParticles = new Particles(CANVAS_WIDTH, CANVAS_HEIGHT)
+
+//    this.image = new Image()
+//     this.image.src = gameObj.src
+//     this.canvasWidth = gameObj.canvasWidth
+//     this.canvasHeight = gameObj.canvasHeight
+//     this.width = gameObj.width
+//     this.height = gameObj.height
+//     this.row = gameObj.row || 20
+//     this.col = gameObj.col || 20
+//     this.tileWidth = this.canvasWidth / row //should nbe 38 for now
+//     this.disableRigidbody = gameObj.disableRigidbody || false
+//     this.frame = 0 //use the first frame if animated
+//     this.isSpikes = false // spike tiles
+//     this.iscollided = false
+let tile = new Tile({
+  src: 'asset/tiles.svg',
+  canvasWidth: CANVAS_WIDTH,
+  canvasHeight: CANVAS_HEIGHT,
+  width: 30,
+  height: 30,
+  x: 100,
+  y: 200,
+  col: 20,
+  row: 20,
+})
 
 function animate(timestamp) {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
@@ -37,3 +62,6 @@ function animate(timestamp) {
 
 // intialize animation
 animate(0)
+
+// intialize a player animate player jumping from down and falling to ground before starting
+function initializePlayer() {}
