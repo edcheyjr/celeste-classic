9788347bd1ebe8d07143fffb050d7d9480871735
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-25 15:22:17 +0300

    player class intial commit

---
+++ src/player.js
@@ -0,0 +1,19 @@
+class Player {
+  constructor() {}
+  draw() {
+    // Draw the player here depending with the current animation playing
+    // ANIMATIONS
+    // blue hair represent dashing mode we active
+    // idle animation
+    // jump animation
+    // walking / running in any direction
+    // climbing
+  }
+  updated() {
+    // The player should keep falling due to gravity
+    // also make sure to check if the player falls beyond canvas width restart the section
+    // the player will also have some special movement jumping dashing and climbing down
+    // a wall while holding dash also this will be dependent on the arrows <-|-> | DOWN | UP
+    //
+  }
+}
