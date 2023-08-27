307d5056a8eac0a1563c99e3f75d93f420bb7913
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-27 14:17:13 +0300

    using params instead of gameObj

--- src/tile.js
+++ src/tile.js
@@ -9,38 +9,46 @@
  */
 class Tile {
   /**
-   * @param {object} gameObj tiles parameter
-   * @param {string } gameObj.src  source of tile image
-   * @param {string } gameObj.canvasWidth Game widht
-   * @param {string } gameObj.canvasHeight Game height
-   * @param {string } gameObj.width Game height
-   * @param {number } gameObj.height Game height
-   * @param {number} gameObj.row Number of tiles from 0 top of the screen to the bottom
-   * @param {number} gameObj.col Number of tiles from 0 right of the screen to the left
-   * @param {number } gameObj.x x position in the canvas
-   * @param {number } gameObj.y y postion in the canvas
+   * @param {object} params tiles parameter
+   * @param {string } params.src  source of tile image
+   * @param {number } params.canvasWidth Game widht
+   * @param {number} params.canvasHeight Game height
+   * @param {number} params.width width of the object
+   * @param {number} params.height height of the object
+   * @param {number} params.row Number of tiles from 0 top of the screen to the bottom
+   * @param {number} params.col Number of tiles from 0 right of the screen to the left
+   * @param {number} params.x x position in the canvas
+   * @param {number} params.y y postion in the canvas
+   * @param {Player} params.player player object
    *
    */
-  constructor(gameObj) {
+  constructor(params) {
     this.image = new Image()
-    this.image.src = gameObj.src
+    this.image.src = params.src
     this.spriteWidth = this.image.width || 16
     this.spriteHeight = this.image.height || 16
-    this.canvasWidth = gameObj.canvasWidth
-    this.canvasHeight = gameObj.canvasHeight
-    this.width = gameObj.width
-    this.height = gameObj.height
-    this.row = gameObj.row || 20
-    this.col = gameObj.col || 20
+    this.canvasWidth = params.canvasWidth
+    this.canvasHeight = params.canvasHeight
+    this.width = params.width
+    this.height = params.height
+    this.row = params.row || 20
+    this.col = params.col || 20
     this.tileWidth = this.canvasWidth / this.row //should nbe 38 for now
-    this.disableRigidbody = gameObj.disableRigidbody || false
+    this.disableRigidbody = params.disableRigidbody || false
     this.frame = 0 //use the first frame if animated
     this.isSpikes = false // spike tiles
     this.iscollided = false
+    this.player = params.player
     // positions
-    this.x = gameObj.x || 0 //todo
-    this.y = gameObj.y || 0 //TODO
+    this.x = params.x || 0 //todo
+    this.y = params.y || 0 //TODO
+    this.getIsCollided = () => this.iscollided
   }
+  /**
+   * draw function
+   * _____________
+   * @param {CanvasRenderingContext2D} ctx context rendering 2D
+   */
   draw(ctx) {
     // Draw a rectangle which will act as the rigidbody mesh also used to determine collisions
     ctx.save()
@@ -62,8 +70,6 @@ class Tile {
     )
   }
   update(deltaTime) {
-    // Add a collusion detection which will determine the igidbody which will be if the player and the tiles have collided and is not spikes
-    //
+    // Any required updates especially tempPlatform
   }
-  // getRectParams
 }
