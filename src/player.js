d64d303be943a5f29e156c3c9f8219be74e841d9
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-29 15:32:56 +0300

    player collusion detection and left right movements

--- src/player.js
+++ src/player.js
@@ -1,6 +1,6 @@
 class Player {
   /**
-   * ------------
+   * --------------
    * Player class
    * --------------
    * @description this is the may blueprint for player object should be a singleton that is only one should ever exist also it should always intialize itself
@@ -15,12 +15,14 @@ class Player {
    * @param {number} params.x x position in the game | canvas
    * @param {number} params.y y postion in the canvas
    * @param {number} params.gravity down force constantly acting on the player
-   * @param {number} params.speed value which will be calcuated as the speed | acceleration in any give direction that the player directs
+   * @param {number} params.friction a opposite force that causes drag and  ulimate stops items for ever moving in one direction
+   * @param {number} params.acceleration value which will be calcuated as the acceleration in any give direction that the player directs
    * @param {number} params.jumpVelocity velocity at which the player can jump up before starting to fall
    * @param {number} params.dashVelocity velocity at which the player can double jump or dash left or right or up midair or on the ground which pressed a key e,g C
    * @param {number} params.climbingVelocity velocity at which the player climbs down or up a "mountain" this should alway be less that the gravity range[0 and 0.9]
    */
   constructor(params) {
+    const minimum = 1
     this.image = new Image()
     this.image.src = params.src
     this.spriteWidth = params.spriteWidth || this.image.width
@@ -29,8 +31,12 @@ class Player {
     this.canvasHeight = params.canvasHeight
     this.width = params.width
     this.height = params.height
-    this.gravity = params.gravity || 1
-    this.speed = params.speed || 2
+    this.gravity = params.gravity || 1 //TODO FUN FACT GRAVITY FOR EARTH
+    this.gravitySpeed = this.gravity * 9.807
+    this.acceleration = params.acceleration || 1
+    this.speed = 0
+    this.jumpVelocity = 15
+    this.friction = params.friction || 1
     this.x = params.x
     this.y = params.y
   }
@@ -44,7 +50,7 @@ class Player {
   draw(ctx) {
     // Draw a rectangle which will act as the rigidbody mesh also used to determine collisions
     ctx.save()
-    ctx.fillStyle = '#a11ad1'
+    ctx.fillStyle = 'gold'
     ctx.fillRect(this.x, this.y, this.width, this.height)
     ctx.restore()
     // Draw the player here depending with the current animation playing
@@ -73,24 +79,30 @@ class Player {
    * _____________________________________________________________________________________________________________________
    * @param {number} deltaTime timeForAnother Animation loop to occur typicly with 16usec for computer on empty animation
    * @param {InputSingleton} input input class provide current input keys
-   * @param {Tile[]} tiles array of all the tiles in the gamescreen //FIXME: potential shows the game do to the large number of tile anyway not bad as the tile are a fixed number always per level just make sure to clear all the tiles object before moving from one level to another
-   *
+   * @param {Tile[][]} tiles tiles objects 2D Array
    */
   update(deltaTime, input, tiles) {
     // deltaTime important in determine refresh rate of the player animations
     // The player should keep falling due to gravity
-    this.y += this.gravity
+    this.y += this.gravitySpeed
+
+    // The player move from left or right
+    this.x += this.speed
     // Add a collusion detection which will determine the rigidbody which will be if the player and the tiles have collided and is not spikes
     this.#hitSideWalls()
     // player should collide with object with rigidBody
-    for (let i = 0; i < tiles.length; i++) {
-      this.#collusionDetectionObjectsWithRigidBody(tiles[i])
-    }
-
-    // also make sure to check if the player falls beyond canvas height restart the section
-    // the player will also have some special movement jumping dashing and climbing down
-    // a wall while holding dash also this will be dependent on the arrows <-|-> | DOWN | UP
-    //
+    // TODO: On^2 which more often never be not be a O of ROWS*COLS^2 is not that bad for know level generation will have to evolve to be more optimized later.
+    tiles.forEach((tileRow) => {
+      if (tileRow.length > 0) {
+        tileRow.forEach((tile) => {
+          if (tile) {
+            this.#collusionDetectionObjectsWithRigidBody(tile)
+          }
+        })
+      }
+    })
+    // added input handling function
+    this.#handlePlayerMovements(input)
   }
 
   #asPlayerClimbedSuccessfully() {
@@ -106,8 +118,8 @@ class Player {
     if (this.x < 0) {
       this.x = 0
     }
-    if (this.x > this.canvasWidth) {
-      this.x = this.canvasWidth - this.width
+    if (this.x >= CANVAS_WIDTH - this.width) {
+      this.x = CANVAS_WIDTH - this.width
     }
   }
   /**
@@ -129,31 +141,37 @@ class Player {
       width: tile.width,
       height: tile.height,
     }
-    console.log('disableRigidbody', tile.disableRigidbody)
+    // console.log('disableRigidbody', tile)
     const isCollided = checkRectangleCollusion(playerRect, tileRect)
+    // console.log('isCollided', isCollided)
     // tile.setIsCollided(isCollided)
 
-    if (isCollided && !tile.disableRigidbody) {
+    if (isCollided && !tile.disableRigidBody) {
       // re-position the player to the top of the tile
-      if (this.y >= tile.y) {
+      if (this.y + this.height >= tile.y) {
+        // console.log('y change')
         this.y == tile.y - this.height
+        // add same acting force on the opposite direction //TODO one can argue why not just set gravity to zero well maybe i'll do that
+        this.y -= this.gravitySpeed
       }
       // also do the same fo the sides if the player hit tile on the side reposition the player just next to the tile
-      // left
-      if (this.x <= tile.x + tile.width) {
+      // // left
+      if (this.x <= tile.x + tile.width && this.y == tile.y) {
         this.x == tile.x + tile.width
       }
       // right
-      if (this.x + this.width >= tile.x) {
+      if (this.x + this.width >= tile.x && this.y == tile.y) {
         this.x = tile.x - this.width
       }
     }
+    // also make sure to check if the player falls beyond canvas height restart the section
     //TODO Check if its gameover move this to
     if (
       (isCollided && !tile.disableRigidbody && tile.isSpikes) ||
       this.#asPlayerFallen()
     ) {
       GAME_OVER = true //TODO use get and setter syntax also get this object from gameobjects
+      // Restart section
     }
   }
   // checks if the player as fallen beyond gamescreen
@@ -164,9 +182,43 @@ class Player {
     return false
   }
 
+  /**
+   *
+   * @param {InputSingleton} input
+   */
   #handlePlayerMovements(input) {
-    // TODO
+    // handle moving right up down left <- ^ -> jump pressing Z up and down to do nothing for now
+    //left right inputs
+    if (
+      input.keys.indexOf(KEY_RIGHT) > -1 ||
+      input.keys.indexOf(SWIPE_RIGHT) > -1
+    ) {
+      this.speed = 5
+    } else if (
+      input.keys.indexOf(KEY_LEFT) > -1 ||
+      input.keys.indexOf(SWIPE_LEFT) > -1
+    ) {
+      this.speed = -5
+    } else {
+      this.speed = 0
+    }
+    // will help in directing angle of jump on vertical walls i.e if the player touch a vertical wall they will jump at an angle 45
+    // handle special movement such as dash climb and double jump
+    // the player will also have some special movement jumping dashing and climbing down
+    // Dash will require  up and down and right for directing the direction of the dash which if executed after a jump will result in a double jump with the second jump dependant on the direction
   }
 }
 
-// Create a singleton for player class to ensure only one instance is currently in user
+//TODO Coyote time
+/**
+ * Coyote Time A brief delay between an action and the consequences of that action that has no physical cause and exists only for comedic or gameplay purposes.
+ *
+ *  + = is the coyote valid area with a limited time to execute an action before physics catches up with you
+ *  _ = is overlap boxes over the real tiles which help add extra pixel of vaild ground // added on all except the ones at the end of the canvas which should be exact to prevent overlap
+ *      _______
+ *  +++|+++++++|+++
+ *     | |---| |
+ *     | |---| |
+ *     |+++++++|
+ *
+ */
