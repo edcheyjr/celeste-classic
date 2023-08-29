63e2448f7cdd7909c4fd8bad3da8b712c1dc3467
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-29 15:33:43 +0300

    change level generator array styles

--- src/levelGenerator.js
+++ src/levelGenerator.js
@@ -4,14 +4,26 @@
  * @param {number[]} level This will be a 1D array with all the values representational of the game objects
  * @param {number} rows Number of rows
  * @param {number} cols  Number of cols
- * @returns {any[][]}
+ * @returns {Tile[][]}
  */
 function levelGenerator(level, rows, cols) {
   const lastItemRowIndex = rows - 1
   const lastItemColIndex = cols - 1
-  const level_2D = Array.from({ length: rows }, () =>
-    new Array(cols).fill(null)
-  ) // Create a 2D array filled with EMPTY (0)
+  const level_2D = Array.from({ length: rows }, () => new Array()) // Create a 2D array filled with EMPTY (0)
+  level_2D[4].push(
+    new Tile({
+      src: 'assets/soil1.png',
+      canvasWidth: CANVAS_WIDTH,
+      canvasHeight: CANVAS_HEIGHT,
+      width: 30,
+      height: 30,
+      x: 10 * lastItemColIndex * 2,
+      y: 4 * lastItemRowIndex * 2,
+      col: cols,
+      row: rows,
+      disableRigidBody: true,
+    })
+  )
   // all the game objects that will be used by the tiles class
   // and their sorces
   for (let i = 0; i < level.length; i++) {
@@ -33,8 +45,9 @@ function levelGenerator(level, rows, cols) {
         y: y * lastItemRowIndex * 2,
         col: cols,
         row: rows,
+        disableRigidBody: true,
       })
-      level_2D[y][x] = underGround
+      level_2D[y].push(underGround)
     }
     if (level[i] == SURFACE) {
       // Add surface
@@ -48,9 +61,10 @@ function levelGenerator(level, rows, cols) {
         y: y * lastItemRowIndex * 2,
         col: cols,
         row: rows,
+        disableRigidBody: false,
       })
-      console.log('2DaRRAY', level_2D)
-      level_2D[y][x] = surface
+      // console.log('2Darray', level_2D)
+      level_2D[y].push(surface)
     }
     // Flowers
     // Grass
